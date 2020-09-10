6934ccb9308cc0f2773742abec18d2f352a11056
oyj <oyj@local>
2020-09-10 18:08:42 +0900

    azure,how to get image list command

---
+++ list-ext.sh
@@ -0,0 +1,51 @@
+oyj@controller:~/prac/ansible-azure$ az vm extension image list-names --location koreacentral --publisher Microsoft.Compute
+[
+  {
+    "computeRole": null,
+    "handlerSchema": null,
+    "id": "/Subscriptions/aa17af3a-aeb2-4c68-801d-595100662a20/Providers/Microsoft.Compute/Locations/KoreaCentral/Publishers/Microsoft.Compute/ArtifactTypes/VMExtension/Types/BGInfo",
+    "location": "KoreaCentral",
+    "name": "BGInfo",
+    "operatingSystem": null,
+    "supportsMultipleExtensions": null,
+    "tags": null,
+    "type": null,
+    "vmScaleSetEnabled": null
+  },
+  {
+    "computeRole": null,
+    "handlerSchema": null,
+    "id": "/Subscriptions/aa17af3a-aeb2-4c68-801d-595100662a20/Providers/Microsoft.Compute/Locations/KoreaCentral/Publishers/Microsoft.Compute/ArtifactTypes/VMExtension/Types/CustomScriptExtension",
+    "location": "KoreaCentral",
+    "name": "CustomScriptExtension",
+    "operatingSystem": null,
+    "supportsMultipleExtensions": null,
+    "tags": null,
+    "type": null,
+    "vmScaleSetEnabled": null
+  },
+  {
+    "computeRole": null,
+    "handlerSchema": null,
+    "id": "/Subscriptions/aa17af3a-aeb2-4c68-801d-595100662a20/Providers/Microsoft.Compute/Locations/KoreaCentral/Publishers/Microsoft.Compute/ArtifactTypes/VMExtension/Types/JsonADDomainExtension",
+    "location": "KoreaCentral",
+    "name": "JsonADDomainExtension",
+    "operatingSystem": null,
+    "supportsMultipleExtensions": null,
+    "tags": null,
+    "type": null,
+    "vmScaleSetEnabled": null
+  },
+  {
+    "computeRole": null,
+    "handlerSchema": null,
+    "id": "/Subscriptions/aa17af3a-aeb2-4c68-801d-595100662a20/Providers/Microsoft.Compute/Locations/KoreaCentral/Publishers/Microsoft.Compute/ArtifactTypes/VMExtension/Types/VMAccessAgent",
+    "location": "KoreaCentral",
+    "name": "VMAccessAgent",
+    "operatingSystem": null,
+    "supportsMultipleExtensions": null,
+    "tags": null,
+    "type": null,
+    "vmScaleSetEnabled": null
+  }
+]
